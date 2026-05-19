69de43259a926213b7f05ea20b9f4d90c1710fd7
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2026-05-19 11:16:35 -0400

    ElasticSearch index mappings

---
+++ backend/storage/elastic_client.py
@@ -0,0 +1,39 @@
+import os
+from elasticsearch import AsyncElasticsearch
+
+INDEX_NAME = "paramount_events"
+
+_client = None
+
+def get_es_client() -> AsyncElasticsearch:
+    global _client
+    if _client is None:
+        _client = AsyncElasticsearch(
+            hosts=[f"https://localhost:9200"],
+            basic_auth=("elastic", os.getenv("ELASTIC_PASSWORD")),
+            verify_certs=False,
+        )
+    return _client
+
+async def create_index(client: AsyncElasticsearch):
+    exists = await client.indices.exists(index=INDEX_NAME)
+    if not exists:
+        await client.indices.create(index=INDEX_NAME, mappings={
+            "properties": {
+                "id":               {"type": "keyword"},
+                "source":           {"type": "keyword"},
+                "release":          {"type": "keyword"},
+                "title":            {"type": "text"},
+                "text":             {"type": "text"},
+                "author":           {"type": "keyword"},
+                "url":              {"type": "keyword"},
+                "timestamp":        {"type": "date"},
+                "sentiment":        {"type": "keyword"},
+                "sentiment_score":  {"type": "float"}
+
+            }
+        })
+        print(f"[ES] Created index '{INDEX_NAME}'")
+    else:
+        print(f"[ES] Index '{INDEX_NAME}' already exists")
+
